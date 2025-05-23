24994ad74f984a6d13337eb3882ff6bf87fd66b5
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-05-23 20:11:14 +0530

    solve navbar problem

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,56 +1,121 @@
-import React, { useRef } from 'react'
-import "./Nav.css"
-import {Link} from "react-scroll"
-import {useGSAP} from "@gsap/react"
-import gsap from 'gsap'
+import React, { useRef } from "react";
+import "./Nav.css";
+import { Link } from "react-scroll";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function Nav() {
-  let menu = useRef()
-  let mobile = useRef()
+  let menu = useRef();
+  let mobile = useRef();
 
-
-  useGSAP(()=>{
-    let t1 = gsap.timeline()
-    t1.from("nav h1",{
-      y:-100,
-      duration:1,
-      opacity:0
-    })
-    t1.from("nav ul li",{
-      y:-100,
-      duration:1,
-      opacity:0,
-      stagger:1
-    })
-  })
+  useGSAP(() => {
+    let t1 = gsap.timeline();
+    t1.from("nav h1", {
+      y: -100,
+      duration: 1,
+      opacity: 0,
+    });
+    t1.from("nav ul li", {
+      y: -100,
+      duration: 1,
+      opacity: 0,
+      stagger: 1,
+    });
+  });
 
   return (
     <nav>
       <h1>PORTPOLIO</h1>
-      <ul className='desktopmenu'>
-        <Link to="home" smooth={true} duration={500} activeClass='active' spy={true}><li>Home</li></Link>
-        <Link to="about" smooth={true} duration={500} activeClass='active' spy={true}><li>About</li></Link>
-        <Link to="projects" smooth={true} duration={500} activeClass='active' spy={true}><li>Projects</li></Link>
-        <Link to="contact" smooth={true} duration={500} activeClass='active' spy={true}><li>Contact</li></Link>
+      <ul className="desktopmenu">
+        <Link
+          to="home"
+          smooth={true}
+          duration={500}
+          activeClass="active"
+          spy={true}
+        >
+          <li>Home</li>
+        </Link>
+        <Link
+          to="about"
+          smooth={true}
+          duration={500}
+          activeClass="active"
+          spy={true}
+        >
+          <li>About</li>
+        </Link>
+        <Link
+          to="projects"
+          smooth={true}
+          duration={500}
+          activeClass="active"
+          spy={true}
+        >
+          <li>Projects</li>
+        </Link>
+        <Link
+          to="contact"
+          smooth={true}
+          duration={500}
+          activeClass="active"
+          spy={true}
+        >
+          <li>Contact</li>
+        </Link>
       </ul>
-      <div className="hamburger" ref={menu} onClick={
-        ()=>{
-          mobile.current.classList.toggle("activemobile")
-          menu.current.classList.toggle("activeham")
-        } 
-      }>
+      <div
+        className="hamburger"
+        ref={menu}
+        onClick={() => {
+          mobile.current.classList.toggle("activemobile");
+          menu.current.classList.toggle("activeham");
+        }}
+      >
         <div className="ham"></div>
         <div className="ham"></div>
         <div className="ham"></div>
       </div>
-      <ul className='mobilemenu' ref={mobile}>
-        <Link to="home" smooth={true} duration={500} activeClass='active' spy={true}><li>Home</li></Link>
-        <Link to="about" smooth={true} duration={500} activeClass='active' spy={true}><li>About</li></Link>
-        <Link to="projects" smooth={true} duration={500} activeClass='active' spy={true}><li>Projects</li></Link>
-        <Link to="contact" smooth={true} duration={500} activeClass='active' spy={true}><li>Contact</li></Link>
+      <ul className="mobilemenu" ref={mobile}>
+        <Link
+          to="home"
+          smooth={true}
+          duration={500}
+          activeClass="active"
+          spy={true}
+        >
+          <li>Home</li>
+        </Link>
+        <Link
+          to="about"
+          smooth={true}
+          duration={500}
+          activeClass="active"
+          spy={true}
+        >
+          <li>About</li>
+        </Link>
+        <Link
+          to="projects"
+          smooth={true}
+          duration={500}
+          activeClass="active"
+          spy={true}
+        >
+          <li>Projects</li>
+        </Link>
+        <Link
+          to="contact"
+          smooth={true}
+          duration={500}
+          activeClass="active"
+          spy={true}
+        >
+          <li>Contact</li>
+        </Link>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
